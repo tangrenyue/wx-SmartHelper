dde02bb72ef4409367152dc7b67f7de216bb60ec
tangrenyue <724562793@qq.com>
2019-05-12 23:00:08 +0800

    menu-weather

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -19,24 +19,49 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    this.updateMenuData()
+   // this.updateMenuData()
   },
-
+  fillIconData: function (data) {
+    var pathPrefix = '../../../resources/icons/application/'
+    for (var index = 0; index < data.length; index++) {
+      var item = data[index]
+      console.log(item.application)
+      switch (item.application) {
+        case 'weather':
+          item.icon = pathPrefix + 'application-weather.png'
+          console.log('weather')
+          break
+        case 'stock':
+          item.icon = pathPrefix + 'application-stock.png'
+          break
+        case 'joke':
+          item.icon = pathPrefix + 'application-joke.png'
+          break
+        case 'constellation':
+          item.icon = pathPrefix + 'application-constellation.png'
+          break
+        case 'backup-image':
+          item.icon = pathPrefix + 'application-images.png'
+          break
+      }
+    }
+    return data
+  },
   /**
    * 请求后台，更新menu数据
    */
-  updateMenuData: function() {
-    var that = this
-    wx.request({
-      url: app.globalData.serverUrl + app.globalData.apiVersion + '/service/menu',
-      success: function(res){
-        var menuData = res.data.data
-        that.setData({
-          grids: menuData
-        })
-      }
-    })
-  },
+  // updateMenuData: function() {
+  //   var that = this
+  //   wx.request({
+  //     url: app.globalData.serverUrl + app.globalData.apiVersion + '/service/menu',
+  //     success: function(res){
+  //       var menuData = res.data.data
+  //       that.setData({
+  //         grids: menuData
+  //       })
+  //     }
+  //   })
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
